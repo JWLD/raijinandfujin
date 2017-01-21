c475da46ba09bb1095b72408119a4d01ae889699
Adam Harwood <adam.harwood@ultrahaptics.com>
2017-01-21 12:05:35 +0000

    Add trigger spawner

---
+++ Assets/Scripts/TriggerSpawner.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerSpawner : MonoBehaviour
+{
+	[SerializeField]
+	int _multiplier16x9;
+	[SerializeField]
+	GameObject _triggerPrefab;
+
+	// Use this for initialization
+	void Start()
+	{
+		var numHorizontal = 16 * _multiplier16x9;
+		var numVertical = 9 * _multiplier16x9;
+
+		var distance = 17.7777777f / numHorizontal;
+		var size = 1f / _multiplier16x9;
+		var horizontalOffset = 8.888888f - (size / 2f);
+		var verticalOffset = 5f - (size / 2f);
+
+		for(var i = 0; i < numHorizontal; i++)
+		{
+			for(var j = 0; j < numVertical; j++)
+			{
+				var position = new Vector3((i * distance) - horizontalOffset, (j * distance) - verticalOffset);
+				var trigger = Instantiate(_triggerPrefab, position, Quaternion.identity, transform);
+				trigger.transform.localScale = new Vector3(size, size, 10f);
+			}
+		}
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
