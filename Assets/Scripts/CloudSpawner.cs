338fc4462a62df19a391f43d38410bf8945f9ee6
JWLD <JackDavies@Jacks-MacBook-Pro-3.local>
2017-01-21 19:09:22 +0000

    stuff

---
+++ Assets/Scripts/CloudSpawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudSpawner : MonoBehaviour
+{
+	public GameObject cloudPrefab;
+	public int spawnGapInSeconds;
+	public int spawnLimit;
+	private int cloudCount;
+
+	void Start()
+	{
+		SpawnNewCloud();
+		StartCoroutine(SpawnTimer());
+	}
+
+	void SpawnNewCloud()
+	{
+		GameObject newCloud = (GameObject)Instantiate(cloudPrefab, transform.position, Quaternion.identity);
+		cloudCount++;
+
+		// add random force
+		newCloud.transform.eulerAngles = new Vector3(0, 0, UnityEngine.Random.Range(0, 360));
+		var force = newCloud.transform.up * UnityEngine.Random.Range(10f, 50f);
+		newCloud.GetComponent<Rigidbody>().AddForce(force);
+	}
+
+	IEnumerator SpawnTimer()
+	{
+		while (cloudCount < spawnLimit)
+		{
+			yield return new WaitForSeconds(spawnGapInSeconds);
+			SpawnNewCloud();
+		}
+	}
+}
