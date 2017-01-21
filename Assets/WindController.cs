f6ef56543235ddf515eccf0d09f8d89fb1a291e0
JWLD <JackDavies@Jacks-MacBook-Pro-3.local>
2017-01-21 16:45:42 +0000

    windtunnels

---
+++ Assets/WindController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindController : MonoBehaviour 
+{
+	[SerializeField]
+	GameObject[] windNodes;
+
+	void Start ()
+	{
+		// point each node to the next
+		for (int i = 0; i < windNodes.Length; i++)
+		{
+			if (i != windNodes.Length - 1)
+			{
+				Vector3 relativePos = windNodes[i+1].transform.position - windNodes[i].transform.position;
+				Quaternion rotation = Quaternion.LookRotation(relativePos, Vector3.down);
+				windNodes[i].transform.rotation = rotation;
+			}
+			else
+			{
+				windNodes[i].transform.rotation = windNodes[i-1].transform.rotation;
+			}
+		}
+	}
+}
---
+++ Assets/WindMover.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindMover : MonoBehaviour
+{
+	[SerializeField]
+	float _forceMultiplier;
+	[SerializeField]
+	[Range(0.0f, 1.0f)]
+	float _forceDrag;
+
+	void OnTriggerEnter(Collider col)
+	{
+		var cloudRB = col.gameObject.GetComponent<Rigidbody>();
+
+		// add drag force
+		var drag = -cloudRB.velocity * _forceDrag;
+		cloudRB.AddForce(drag, ForceMode.Impulse);
+
+		// add wind force
+		var force = transform.forward.normalized * _forceMultiplier;
+		cloudRB.AddForce(force);
+	}
+}
