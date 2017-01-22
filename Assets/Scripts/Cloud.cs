8566caaf29582c8378f4d20c2abc6b2844646ae9
Adam Harwood <adam.harwood@ultrahaptics.com>
2017-01-22 15:25:50 +0000

    Add louder drum sound

--- Assets/Scripts/Cloud.cs
+++ Assets/Scripts/Cloud.cs
@@ -37,7 +37,7 @@ public class Cloud : MonoBehaviour
 		var timeLeft = _fadeInTime;
 		while(timeLeft > 0f)
 		{
-			_audioSource.volume = Mathf.Lerp(0f, 0.3f, 1f - (timeLeft / _fadeInTime));
+			_audioSource.volume = Mathf.Lerp(0f, 0.1f, 1f - (timeLeft / _fadeInTime));
 			timeLeft -= Time.deltaTime;
 			yield return null;
 		}
@@ -53,7 +53,7 @@ public class Cloud : MonoBehaviour
 		var timeLeft = _fadeOutTime;
 		while(timeLeft > 0f)
 		{
-			_audioSource.volume = Mathf.Lerp(0.3f, 0f, 1f - (timeLeft / _fadeOutTime));
+			_audioSource.volume = Mathf.Lerp(0.1f, 0f, 1f - (timeLeft / _fadeOutTime));
 			timeLeft -= Time.deltaTime;
 			yield return null;
 		}
